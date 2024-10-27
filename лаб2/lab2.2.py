798500ba043b762d6917e4c49d5da81acf32f752
yuliya-voicihovskaya <yuliyavoicihovskaya@icloud.com>
2024-10-27 18:42:03 +0300

    Add files via upload

---
+++ лаб2/lab2.2.py
@@ -0,0 +1,17 @@
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+months = 10  # Количество месяцев, которое планируется протянуть без долгов
+increase = 0.03  # Ежемесячный рост цен
+
+money_capital = 0
+for month in range(months):
+    money = spend - salary
+    if money > 0:
+        money_capital += money
+        spend *= (1 + increase)
+
+# TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
+
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", round(money_capital))
+
+
