a582ad009e41de900747e91fd7552c975a6123d1
yuliya-voicihovskaya <yuliyavoicihovskaya@icloud.com>
2025-01-20 21:55:36 +0300

    Update main1.py

--- лаб2.2/main1.py
+++ лаб2.2/main1.py
@@ -11,19 +11,47 @@ BOOKS_DATABASE = [
     }
 ]
 
+
 class Book:
+    """
+    Класс, представляющий книгу.
+
+    Атрибуты:
+        id (int): Уникальный идентификатор книги.
+        name (str): Название книги.
+        pages (int): Количество страниц в книге.
+    """
+
     def __init__(self, id_, name, pages):
+        """
+        Инициализация экземпляра книги.
+
+        Параметры:
+            id_ (int): Уникальный идентификатор книги.
+            name (str): Название книги.
+            pages (int): Количество страниц в книге.
+        """
         self.id = id_
         self.name = name
         self.pages = pages
 
     def __str__(self):
+        """
+        Возвращает строковое представление книги для вывода.
+
+        Возвращает:
+            str: Строка, содержащая название книги.
+        """
         return f'Книга "{self.name}"'
 
     def __repr__(self):
-        return f'Book(id_={self.id}, name={self.name!r}, pages={self.pages})'
+        """
+        Возвращает неформальное представление книги для отладки.
 
-# TODO написать класс Book
+        Возвращает:
+            str: Строка с полями книги.
+        """
+        return f'Book(id_={self.id}, name={self.name!r}, pages={self.pages})'
 
 
 if __name__ == '__main__':
@@ -31,11 +59,8 @@ if __name__ == '__main__':
     list_books = [
         Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
     ]
+
     for book in list_books:
         print(book)  # проверяем метод str
 
     print(list_books)  # проверяем метод repr
-
-
-
-
